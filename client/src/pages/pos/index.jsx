3b6a7c4213ff5a43cf89a9bb7aea8d34b888f92a
Thafsi PV <pvthafsi@gmail.com>
2023-08-13 08:39:35 +0530

    lay out rearrange

--- client/src/pages/pos/index.jsx
+++ client/src/pages/pos/index.jsx
@@ -22,101 +22,108 @@ function index() {
   const openModal = (item) => {
     setIsModalOpen(true);
     setModalItem(item);
-    document.body.classList.add("modal-open"); // Add class to body
+    document.body.classList.add("modal-open"); // Add className to body
   };
 
   const closeModal = () => {
     setIsModalOpen(false);
-    document.body.classList.remove("modal-open"); // Remove class from body
+    document.body.classList.remove("modal-open"); // Remove className from body
   };
 
   return (
     <div className="w-full md:h-[500px] sm:h-[100px] lg:h-[700px] 2xl:h-[650px] 3xl:h-[700px]  overflow-hidden mt-5">
-      <div class="grid grid-cols-5 gap-4 relative h-full  dark:!bg-navy-900 ">
-        <Card extra={"w-full h-full sm:overflow-auto p-6 col-span-3"}>
-          <div class=" overflow-y-scroll">
-            <div className="w-full gap-2 flex sticky top-0 z-10 bg-white dark:!bg-navy-900  pb-3 rounded-xl">
-              <div className="flex w-full">
-                <DropDownReactSelect ph="Select Table" />
-              </div>
-              <div className="flex w-full">
-                <DropDownReactSelect ph="Select Category" />
-              </div>
-              <div className="flex w-full">
-                <DropDownReactSelect ph="Select Customer" />
-              </div>
-            </div>
-            <div className="w-full flex flex-wrap  h-full">
-              {menu.restaurant_items.map((item) => {
-                return (
-                  <div
-                    key={item.id}
-                    className="w-1/6 p-2 cursor-pointer"
-                    onClick={() => openModal(item)}>
-                    <ImageCard item={item} />
+      <div className="grid grid-cols-5 grid-rows-6 gap-4 relative h-full  dark:!bg-navy-900 ">
+        <div className="w-full h-full  col-span-3 row-span-6">
+          <div className="grid grid-rows-6">
+            <Card extra={"w-full h-full sm:overflow-auto p-6 row-span-6"}>
+              {/*product list div */}
+              <div className=" overflow-y-scroll">
+                <div className="w-full gap-2 flex sticky top-0 z-10 bg-white dark:!bg-navy-900  pb-3 rounded-xl">
+                  <div className="flex w-full">
+                    <DropDownReactSelect ph="Select Table" />
                   </div>
-                );
-              })}
+                  <div className="flex w-full">
+                    <DropDownReactSelect ph="Select Category" />
+                  </div>
+                  <div className="flex w-full">
+                    <DropDownReactSelect ph="Select Customer" />
+                  </div>
+                </div>
+                <div className="w-full flex flex-wrap  h-full">
+                  {menu.restaurant_items.map((item) => {
+                    return (
+                      <div
+                        key={item.id}
+                        className="w-1/6 p-2 cursor-pointer"
+                        onClick={() => openModal(item)}>
+                        <ImageCard item={item} />
+                      </div>
+                    );
+                  })}
 
-              {/* <div className="w-1/6 p-2">
+                  {/* <div className="w-1/6 p-2">
                 <ImageCard />
               </div> */}
+                </div>
+              </div>
+            </Card>
+            <div className=" row-span-1">action div</div>
+          </div>
+        </div>
+        <div className=" col-span-2 row-span-6 h-full">
+          {/*selected items div */}
+          <div className="grid grid-rows-6 h-full gap-4">
+            <div className="overflow-y-scroll row-span-6 ">
+              <SelectedItemsTable
+                cartItems={cartItems}
+                selectedItemListRef={selectedItemListRef}
+              />
+            </div>
+            <div className="">
+              {/*summary div */}
+              <Card extra={"w-full h-full sm:overflow-auto p-6 pt-2"}>
+                <header className="relative flex items-center justify-between pt-4">
+                  <div className="text-xl font-bold text-navy-700 dark:text-white w-full text-right">
+                    <table className="w-full p-3 pb-3">
+                      <tr className="text-sm">
+                        <td>
+                          <p>Discount:</p>
+                        </td>
+                        <td>00.00</td>
+                      </tr>
+                      <tr className="text-sm">
+                        <td>
+                          <p>Total Tax:</p>
+                        </td>
+                        <td>12.50</td>
+                      </tr>
+                      <tfoot className="border-t">
+                        <tr>
+                          <td>Total Payable:</td>
+                          <td>258.00</td>
+                        </tr>
+                      </tfoot>
+                    </table>
+                  </div>
+                </header>
+                <div className="flex justify-center w-full gap-2 pt-3">
+                  <div className="flex-1">
+                    <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full w-full flex justify-center items-center gap-2">
+                      <FaRupeeSign />
+                      Cash
+                    </button>
+                  </div>
+                  <div className="flex-1">
+                    <button
+                      className="bg-green-400 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full w-full flex justify-center items-center gap-2"
+                      onClick={() => handlePayment(280.5)}>
+                      <BsFillCreditCardFill /> <p>Card</p>
+                    </button>
+                  </div>
+                </div>
+              </Card>
             </div>
           </div>
-        </Card>
-        <div class="col-span-2 overflow-y-scroll">
-          {/* <CheckTable
-            columnsData={columnsDataCheck}
-            tableData={tableDataCheck}
-          /> */}
-          <SelectedItemsTable
-            cartItems={cartItems}
-            selectedItemListRef={selectedItemListRef}
-          />
-        </div>
-        <div className="col-span-3"></div>
-        <div className="col-span-2">
-          <Card extra={"w-full h-full sm:overflow-auto p-6 pt-2"}>
-            <header className="relative flex items-center justify-between pt-4">
-              <div className="text-xl font-bold text-navy-700 dark:text-white w-full text-right">
-                <table className="w-full p-3 pb-3">
-                  <tr className="text-sm">
-                    <td>
-                      <p>Discount:</p>
-                    </td>
-                    <td>00.00</td>
-                  </tr>
-                  <tr className="text-sm">
-                    <td>
-                      <p>Total Tax:</p>
-                    </td>
-                    <td>12.50</td>
-                  </tr>
-                  <tfoot className="border-t">
-                    <tr>
-                      <td>Total Payable:</td>
-                      <td>258.00</td>
-                    </tr>
-                  </tfoot>
-                </table>
-              </div>
-            </header>
-            <div className="flex justify-center w-full gap-2 pt-3">
-              <div className="flex-1">
-                <button class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full w-full flex justify-center items-center gap-2">
-                  <FaRupeeSign />
-                  Cash
-                </button>
-              </div>
-              <div className="flex-1">
-                <button
-                  class="bg-green-400 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full w-full flex justify-center items-center gap-2"
-                  onClick={() => handlePayment(280.5)}>
-                  <BsFillCreditCardFill /> <p>Card</p>
-                </button>
-              </div>
-            </div>
-          </Card>
         </div>
       </div>
       {/* Render the modal component */}
