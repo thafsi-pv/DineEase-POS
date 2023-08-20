b5730e053215261716108704a407efac52fa4dd1
Thafsi PV <pvthafsi@gmail.com>
2023-08-20 21:08:00 +0530

    chat ui

--- client/src/pages/chat/Chat.jsx
+++ client/src/pages/chat/Chat.jsx
@@ -1,7 +1,68 @@
 import React from "react";
 
 function chat() {
-  return <div>chat</div>;
+  return (
+    <div className="w-full h-screen flex bg-gray-100">
+      <ContactList />
+      <div className="flex-grow flex flex-col">
+        <div className="overflow-y-auto p-4">
+          <ChatMessage message="Hey, how's it going?" isMine={false} />
+          <ChatMessage
+            message="I'm good, thanks! How about you?"
+            isMine={true}
+          />
+          {/* Add more messages here */}
+        </div>
+        <div className="bg-white p-4 border-t">
+          <input
+            type="text"
+            placeholder="Type a message..."
+            className="w-full p-2 border rounded-md"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default chat;
+
+const ChatMessage = ({ message, isMine }) => {
+  return (
+    <div
+      className={`flex ${
+        isMine ? "justify-end" : "justify-start"
+      } mb-4 items-end`}>
+      <div
+        className={`max-w-xs p-3 rounded-lg ${
+          isMine ? "bg-blue-500 text-white" : "bg-gray-200"
+        }`}>
+        {message}
+      </div>
+    </div>
+  );
+};
+
+const ContactList = () => {
+  const contacts = [
+    { id: 1, name: "John Doe" },
+    { id: 2, name: "Jane Smith" },
+    { id: 3, name: "Michael Johnson" },
+    // Add more contacts
+  ];
+
+  return (
+    <div className="w-1/4 bg-gray-200 p-4 overflow-y-auto">
+      <h2 className="text-lg font-semibold mb-2">Contacts</h2>
+      <ul>
+        {contacts.map((contact) => (
+          <li
+            key={contact.id}
+            className="cursor-pointer py-2 hover:bg-gray-300">
+            {contact.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
