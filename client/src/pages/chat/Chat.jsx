bfc573d33d941bd2aa608451b00ceb959b403c0b
Thafsi PV <pvthafsi@gmail.com>
2023-09-03 10:54:24 +0530

    chat ui

--- client/src/pages/chat/Chat.jsx
+++ client/src/pages/chat/Chat.jsx
@@ -3,6 +3,7 @@ import { RiSendPlaneFill } from "react-icons/ri";
 import io from "socket.io-client";
 import incomingNotificationSound from "../../assets/sounds/incomingNotification.mp3";
 import { Howl, Howler } from "howler";
+import Card from "../../components/card";
 Howler.volume(1);
 
 let data = JSON.parse(localStorage.getItem("DEPOS"));
@@ -16,11 +17,9 @@ const sound = new Howl({
 function chat() {
   const [username, setUsername] = useState("");
   const [userList, setUserList] = useState([]);
-  console.log("🚀 ~ file: chat.jsx:12 ~ chat ~ userList:", userList);
   const [selectedRecipient, setSelectedRecipient] = useState("");
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-  console.log("🚀 ~ file: chat.jsx:13 ~ chat ~ messages:", messages);
 
   useEffect(() => {
     socket.emit("login", myUserName);
@@ -49,8 +48,6 @@ function chat() {
         message,
       });
       setMessages((prevMessages) => [
-        // ...prevMessages,
-        // `You to ${selectedRecipient}: ${message}`,
         ...prevMessages,
         { sender: myUserName, recipient: selectedRecipient, message: message },
       ]);
@@ -60,52 +57,54 @@ function chat() {
   };
 
   return (
-    <div className=" h-[75vh] flex bg-gray-100 rounded-lg mt-5 justify-center">
-      <ContactList userList={userList} recipient={setSelectedRecipient} />
-      <div className="flex-grow flex flex-col  items-center">
-        <div className="bg-green-200 w-full p-3 flex">
-          <div className="flex items-center">
-            <img
-              className="w-10 h-10 rounded-full"
-              src="/src/assets/img/avatars/avatar4.png"
-              alt=""
-            />
-            <p className="p-2 font-semibold">{selectedRecipient}</p>
-          </div>
-        </div>
-        <div className="h-full flex-grow md:w-2/3 sm:w-full flex  flex-col  ">
-          <div className="overflow-y-auto p-4 flex-1">
-            <ChatMessage message="Hey, how's it going?" isMine={false} />
-            <ChatMessage
-              message="I'm good, thanks! How about you?"
-              isMine={true}
-            />
-            {messages.map((msg) => (
-              <ChatMessage
-                message={msg.message}
-                isMine={msg.sender == myUserName ? true : false}
+    <Card extra={"w-full h-full p-3 pt-4"}>
+      <div className=" h-[75vh] flex bg-gray-100 rounded-lg  justify-center">
+        <ContactList userList={userList} recipient={setSelectedRecipient} />
+        <div className="flex-grow flex flex-col  items-center">
+          <div className="bg-green-200 w-full p-3 flex">
+            <div className="flex items-center">
+              <img
+                className="w-10 h-10 rounded-full"
+                src="/src/assets/img/avatars/avatar4.png"
+                alt=""
               />
-            ))}
-            {/* Add more messages here */}
+              <p className="p-2 font-semibold">{selectedRecipient}</p>
+            </div>
           </div>
-          <div className="p-4 border-t flex gap-2">
-            <input
-              type="text"
-              placeholder="Type a message..."
-              className="w-full p-2 border rounded-md"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-            />
-            <button
-              type="button"
-              className="p-4 rounded-full border bg-white hover:bg-gray-400"
-              onClick={handleSendMessage}>
-              <RiSendPlaneFill className="h-7 w-7 txtGreenColor" />
-            </button>
+          <div className="h-full flex-grow md:w-2/3 sm:w-full flex  flex-col  ">
+            <div className="overflow-y-auto p-4 flex-1">
+              <ChatMessage message="Hey, how's it going?" isMine={false} />
+              <ChatMessage
+                message="I'm good, thanks! How about you?"
+                isMine={true}
+              />
+              {messages.map((msg) => (
+                <ChatMessage
+                  message={msg.message}
+                  isMine={msg.sender == myUserName ? true : false}
+                />
+              ))}
+              {/* Add more messages here */}
+            </div>
+            <div className="p-4 border-t flex gap-2">
+              <input
+                type="text"
+                placeholder="Type a message..."
+                className="w-full p-2 border rounded-md"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
+              <button
+                type="button"
+                className="p-4 rounded-full border bg-white hover:bg-gray-400"
+                onClick={handleSendMessage}>
+                <RiSendPlaneFill className="h-7 w-7 txtGreenColor" />
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Card>
   );
 }
 
