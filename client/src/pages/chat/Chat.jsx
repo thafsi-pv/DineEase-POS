e42f6163201734ea29a12ca276901a257d017d06
Thafsi PV <pvthafsi@gmail.com>
2023-09-05 00:27:50 +0530

    chat ui

--- client/src/pages/chat/Chat.jsx
+++ client/src/pages/chat/Chat.jsx
@@ -122,61 +122,66 @@ function chat() {
     <Card extra={"w-full h-full p-3 pt-4"}>
       <div className=" h-[75vh] flex bg-gray-100 rounded-lg  justify-center">
         <ContactList userList={userList} recipient={setSelectedRecipient} />
-        <div className="flex-grow flex flex-col  items-center">
-          <div className="bg-green-200 w-full p-3 flex">
-            <div className="flex items-center">
-              <img
-                className="w-10 h-10 rounded-full"
-                src="/src/assets/img/avatars/avatar4.png"
-                alt=""
-              />
-              <p className="p-2 font-semibold">{selectedRecipient}</p>
-            </div>
-          </div>
-          <div className=" h-1/3 flex-grow md:w-2/3 sm:w-full flex  flex-col  ">
-            <div className="overflow-y-scroll p-4 flex-1" ref={chatListRef}>
-              {/* <ChatMessage message="Hey, how's it going?" isMine={false} />
+
+        <div className="flex-grow flex">
+          {selectedRecipient && (
+            <div className="w-full flex-grow flex flex-col  items-center">
+              <div className="bg-green-200 w-full p-3 flex">
+                <div className="flex items-center">
+                  <img
+                    className="w-10 h-10 rounded-full"
+                    src="/src/assets/img/avatars/avatar4.png"
+                    alt=""
+                  />
+                  <p className="p-2 font-semibold">{selectedRecipient}</p>
+                </div>
+              </div>
+              <div className=" h-1/3 flex-grow md:w-2/3 sm:w-full flex  flex-col  ">
+                <div className="overflow-y-scroll p-4 flex-1" ref={chatListRef}>
+                  {/* <ChatMessage message="Hey, how's it going?" isMine={false} />
               <ChatMessage
                 message="I'm good, thanks! How about you?"
                 isMine={true}
               /> */}
-              {messages.map((msg) => (
-                <ChatMessage
-                  message={msg.message}
-                  isMine={msg.sender == myUserName ? true : false}
-                />
-              ))}
-              {/* Add more messages here */}
-            </div>
-            <div className=" p-4 border-t flex items-center gap-2">
-              {showEmoji && (
-                <div className="absolute bottom-24">
-                  <EmojiPicker
-                    onEmojiClick={onEmojiClick}
-                    disableAutoFocus={true}
-                    native
-                  />
+                  {messages.map((msg) => (
+                    <ChatMessage
+                      message={msg.message}
+                      isMine={msg.sender == myUserName ? true : false}
+                    />
+                  ))}
+                  {/* Add more messages here */}
                 </div>
-              )}
-              <BsFillEmojiSmileFill
-                className="h-7 w-7 txtGreenColor"
-                onClick={() => setshowEmoji(true)}
-              />
-              <input
-                type="text"
-                placeholder="Type a message..."
-                className="w-full p-4 border rounded-md flex-1"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-              />
-              <button
-                type="button"
-                className="p-4 rounded-full border bg-white hover:bg-gray-400"
-                onClick={handleSendMessage}>
-                <RiSendPlaneFill className="h-7 w-7 txtGreenColor" />
-              </button>
+                <div className=" p-4 border-t flex items-center gap-2">
+                  {showEmoji && (
+                    <div className="absolute bottom-24">
+                      <EmojiPicker
+                        onEmojiClick={onEmojiClick}
+                        disableAutoFocus={true}
+                        native
+                      />
+                    </div>
+                  )}
+                  <BsFillEmojiSmileFill
+                    className="h-7 w-7 txtGreenColor"
+                    onClick={() => setshowEmoji(true)}
+                  />
+                  <input
+                    type="text"
+                    placeholder="Type a message..."
+                    className="w-full p-4 border rounded-md flex-1"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                  />
+                  <button
+                    type="button"
+                    className="p-4 rounded-full border bg-white hover:bg-gray-400"
+                    onClick={handleSendMessage}>
+                    <RiSendPlaneFill className="h-7 w-7 txtGreenColor" />
+                  </button>
+                </div>
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
     </Card>
@@ -205,7 +210,7 @@ const ContactList = ({ userList, recipient }) => {
   //const [userList, setuserList] = useState([]);
   const list = userList.filter((item) => item.username != myUserName);
   return (
-    <div className="w-1/4 bg-gray-200 p-4 overflow-y-auto">
+    <div className="w-1/4 bg-gray-200  p-4 overflow-y-auto">
       <h2 className="text-lg font-semibold mb-2">Contacts</h2>
       <ul>
         {list.map((user) => (
