315731433d6c25899ca5d2ee20f40c9e796f91cd
Thafsi PV <pvthafsi@gmail.com>
2023-09-07 21:01:01 +0530

    product add form

---
+++ client/src/components/modal/FormModal.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+function FormModal() {
+  const [isModalOpen, setIsModalOpen] = useState(isOpen);
+
+  // Function to close the modal
+  const closeModal = () => {
+    setIsModalOpen(false);
+    onClose();
+  };
+
+  return (
+    <>
+      {isModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="fixed inset-0 bg-black opacity-50"></div>
+          <div className="relative z-10 bg-white p-6 rounded-lg shadow-md">
+            <div className="flex justify-end">
+              <button
+                className="text-gray-600 hover:text-gray-800"
+                onClick={closeModal}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+            <div className="mt-4">{children}</div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default FormModal;
