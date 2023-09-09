4b6f46dc1b46f4f9f2721d09e86c8791e19654e6
Thafsi PV <pvthafsi@gmail.com>
2023-09-09 17:31:51 +0530

    modal animation

--- client/src/components/modal/FormModal.jsx
+++ client/src/components/modal/FormModal.jsx
@@ -1,34 +1,44 @@
-import React, { useState } from "react";
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import { IoIosCloseCircleOutline } from "react-icons/io";
 
 function FormModal({ isOpen, onClose, children, modalWidth }) {
-  // Function to close the modal
-  //   const closeModal = () => {
-  //     setIsModalOpen(false);
-  //     onClose();
-  //   };
-
+  const modalVariants = {
+    hidden: { opacity: 0, y: "-100%" }, // Slide down and fade out
+    visible: { opacity: 1, y: "0%" }, // Slide up and fade in
+  };
+  const modalTransition = {
+    duration: 0.5, // Adjust the duration (in seconds) to control speed
+    ease: "easeInOut", // Adjust the easing function
+  };
   return (
-    <>
+    <AnimatePresence>
       {isOpen && (
-        <div className=" fixed inset-0 flex items-center justify-center z-50 bg-[#00000059] overflow-hidden ">
-          <div className=" inset-0 bg-black opacity-50"></div>
-          <div className="relative z-10 bg-white p-6 rounded-3xl shadow-md">
-            <div className="flex justify-end">
-              <div className="relative -top-8 -right-8 h-fit bg-white rounded-full shadow-lg">
-                <a href="#" onClick={onClose}>
-                  <IoIosCloseCircleOutline className="h-7 w-7 text-gray-600 hover:text-gray-800" />
-                </a>
+        <div className="fixed inset-0 flex items-center justify-center z-50 bg-[#00000059] overflow-hidden">
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            transition={modalTransition}
+            exit="hidden"
+            variants={modalVariants}>
+            <div className="inset-0 bg-black opacity-50"></div>
+            <div className="relative z-10 bg-white p-6 rounded-3xl shadow-md">
+              <div className="flex justify-end">
+                <div className="relative -top-8 -right-8 h-fit bg-white rounded-full shadow-lg">
+                  <a href="#" onClick={onClose}>
+                    <IoIosCloseCircleOutline className="h-7 w-7 text-gray-600 hover:text-gray-800" />
+                  </a>
+                </div>
+              </div>
+              <div
+                className={`mt-4 max-h-[80vh] overflow-y-auto w-[${modalWidth}]`}>
+                {children}
               </div>
             </div>
-            <div
-              className={`mt-4 max-h-[80vh] overflow-y-auto w-[${modalWidth}]`}>
-              {children}
-            </div>
-          </div>
+          </motion.div>
         </div>
       )}
-    </>
+    </AnimatePresence>
   );
 }
 
