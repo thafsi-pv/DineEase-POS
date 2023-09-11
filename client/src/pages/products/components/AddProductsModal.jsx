8d228c080927cd104925f9c3345963cb7a5ea1ef
Thafsi PV <pvthafsi@gmail.com>
2023-09-11 11:15:21 +0530

    add portion

--- client/src/pages/products/components/AddProductsModal.jsx
+++ client/src/pages/products/components/AddProductsModal.jsx
@@ -71,7 +71,8 @@ function AddProductsModal() {
   };
 
   const productValidationSchema = Yup.object().shape({
-    itemName: Yup.string().required("Portion is required"),
+    itemName: Yup.string().required("Item name is required"),
+    price: Yup.number().required("Price is required").min(0),
     category: Yup.number().required("category is required"),
     imageUrl: Yup.string().required("Image is required"),
   });
@@ -100,105 +101,122 @@ function AddProductsModal() {
   });
 
   const handleHasPortionsChange = (e) => {
-    console.log(
-      "🚀 ~ file: AddProductsModal.jsx:107 ~ handleHasPortionsChange ~ e:",
-      e.target.checked
-    );
     productFormik.setFieldValue("hasPortions", e.target.checked);
   };
 
   return (
     <div>
-      {/* <form onSubmit={productFormik.handleSubmit}> */}
-      <div className="flex columns-2 justify-center w-full gap-3">
-        <div className="w-1/2 p-2">
-          <div className="mb-2">
-            <InputField
-              label="Item Name"
-              variant=""
-              id="itemName"
-              extra=""
-              type="text"
-              placeholder="Enter Item Name"
-              onChange={productFormik.handleChange}
-              onBlur={productFormik.handleBlur}
-              value={productFormik.values.itemName}
-              state={
-                productFormik.touched.itemName && productFormik.errors.itemName
-                  ? "error"
-                  : "success"
-              }
-            />
-          </div>
-          <div className="mb-2">
-            <InputField
-              label="Price"
-              variant=""
-              id="itemPrice"
-              extra=""
-              type="number"
-              placeholder="Enter Item Price"
-            />
-          </div>
-          <div className="mb-4">
-            <DropDownReactSelect
-              ph="Select Category"
-              label="Category"
-              id="ddlCategory"
-            />
-          </div>
-          <div className="mb-4">
-            <DropDownReactSelect
-              ph="Select Cusine"
-              label="Cusine"
-              id="ddlCusine"
-              isMulti={true}
-            />
+      <form onSubmit={productFormik.handleSubmit}>
+        <div className="flex columns-2 justify-center w-full gap-3">
+          <div className="w-1/2 p-2 border-r-2">
+            <div className="mb-2">
+              <InputField
+                label="Item Name"
+                variant=""
+                id="itemName"
+                extra=""
+                type="text"
+                placeholder="Enter Item Name"
+                onChange={productFormik.handleChange}
+                onBlur={productFormik.handleBlur}
+                value={productFormik.values.itemName}
+                state={
+                  productFormik.touched.itemName &&
+                  productFormik.errors.itemName
+                    ? "error"
+                    : "success"
+                }
+              />
+              {productFormik.touched.itemName &&
+              productFormik.errors.itemName ? (
+                <div className="text-red-500 text-xs float-right animate-pulse">
+                  {productFormik.errors.itemName}
+                </div>
+              ) : null}
+            </div>
+            <div className="mb-2">
+              <InputField
+                label="Price"
+                variant=""
+                id="itemPrice"
+                extra=""
+                type="number"
+                placeholder="Enter Item Price"
+                onChange={productFormik.handleChange}
+                onBlur={productFormik.handleBlur}
+                value={productFormik.values.price}
+                state={
+                  productFormik.touched.price && productFormik.errors.price
+                    ? "error"
+                    : "success"
+                }
+              />
+              {productFormik.touched.price &&
+              productFormik.errors.price ? (
+                <div className="text-red-500 text-xs float-right animate-pulse">
+                  {productFormik.errors.price}
+                </div>
+              ) : null}
+            </div>
+            <div className="mb-4">
+              <DropDownReactSelect
+                ph="Select Category"
+                label="Category"
+                id="ddlCategory"
+              />
+            </div>
+            <div className="mb-4">
+              <DropDownReactSelect
+                ph="Select Cuisine"
+                label="Cuisine"
+                id="ddlCuisine"
+                isMulti={true}
+              />
+            </div>
+
+            <div class="hs-tooltip flex items-center">
+              <SwithField
+                color="green"
+                label="Active"
+                desc="Product is currently serving or not"
+              />
+            </div>
+            <div class="mt-3">
+              <TextField extra="w-full" color="green" label="Remarks" />
+            </div>
           </div>
 
-          <div class="hs-tooltip flex items-center">
-            <SwithField
-              color="green"
-              label="Active"
-              desc="Product is currently serving or not"
-            />
-          </div>
-          <div class="mt-3">
-            <TextField extra="w-full" color="green" label="Remarks" />
+          <div className="w-1/2">
+            <div class="hs-tooltip flex items-center">
+              <SwitchField
+                color="green"
+                label="Has Portions"
+                desc="Add required portion if available"
+                onChange={handleHasPortionsChange}
+                value={productFormik.values.hasPortions}
+              />
+            </div>
+            {/* //portion component */}
+            {productFormik.values.hasPortions && (
+              <AddPortion portions={portions} setPortions={setPortions} />
+            )}
+            <FileInput inputLabel="Select Image" />
           </div>
         </div>
 
-        <div className="w-1/2">
-          <div class="hs-tooltip flex items-center">
-            <SwitchField
-              color="green"
-              label="Has Portions"
-              desc="Add required portion if available"
-              onChange={handleHasPortionsChange}
-              value={productFormik.values.hasPortions}
-            />
-          </div>
-          {/* //portion component */}
-          {productFormik.values.hasPortions && (
-            <AddPortion portions={portions} setPortions={setPortions} />
-          )}
-          <FileInput inputLabel="Select Image" />
+        <div className="mt-3 float-right flex gap-2">
+          <button
+            type="submit"
+            className="bg-gray-500 text-white py-2 px-4 rounded-md hover:bg-blue-600">
+            Cancel
+          </button>
+          <button
+            type="submit"
+            className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600">
+            Save
+          </button>
         </div>
-      </div>
-
-      <div className="mt-3 float-right flex gap-2">
-        <button
-          type="submit"
-          className="bg-gray-500 text-white py-2 px-4 rounded-md hover:bg-blue-600">
-          Cancel
-        </button>
-        <button
-          type="submit"
-          className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600">
-          Save
-        </button>
-      </div>
-      {/* </form> */}
+      </form>
     </div>
   );
 }
