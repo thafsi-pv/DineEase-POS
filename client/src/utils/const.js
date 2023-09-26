5b60aab6680fa0840711afb23e49bd9012044a91
Thafsi PV <pvthafsi@gmail.com>
2023-09-26 23:38:56 +0530

    render url

--- client/src/utils/const.js
+++ client/src/utils/const.js
@@ -1,13 +1,28 @@
-export const CLOUDINARY_IMAGE_UPLOAD_URL =
+// export const CLOUDINARY_IMAGE_UPLOAD_URL =
+//   "https://api.cloudinary.com/v1_1/dm4djc1b1/image/upload";
+
+// export const PRODUCT_ADD_API = "http://localhost:8080/api/product/create";
+
+// export const GET_ALL_ACTIVE_PRODUCT_API =
+//   "http://localhost:8080/api/product/getAllActiveProducts";
+
+// export const GET_PRODUCT_BY_ID =
+//   "http://localhost:8080/api/product/getProductById";
+
+// export const DELETE_PRODUCT_BY_ID =
+//   "http://localhost:8080/api/product/deleteProductById";
+
+
+  export const CLOUDINARY_IMAGE_UPLOAD_URL =
   "https://api.cloudinary.com/v1_1/dm4djc1b1/image/upload";
 
-export const PRODUCT_ADD_API = "http://localhost:8080/api/product/create";
+export const PRODUCT_ADD_API = "https://dine-ease-api.onrender.com/api/product/create";
 
 export const GET_ALL_ACTIVE_PRODUCT_API =
-  "http://localhost:8080/api/product/getAllActiveProducts";
+  "https://dine-ease-api.onrender.com/api/product/getAllActiveProducts";
 
 export const GET_PRODUCT_BY_ID =
-  "http://localhost:8080/api/product/getProductById";
+  "https://dine-ease-api.onrender.com/api/product/getProductById";
 
 export const DELETE_PRODUCT_BY_ID =
-  "http://localhost:8080/api/product/deleteProductById";
+  "https://dine-ease-api.onrender.com/api/product/deleteProductById";
